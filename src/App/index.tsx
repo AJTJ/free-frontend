2aed9a27df2739c79cb43862caf37a7c8e98b54d
AJTJ <janke.aaron@gmail.com>
2021-09-09 22:21:07 -0700

    updated for cookies

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,44 +1,94 @@
 import React from "react";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql,
-} from "@apollo/client";
+import { useQuery, useMutation, gql } from "@apollo/client";
 
-const GetAllUsers = gql`
-  query UsersService {
+const GET_ALL_USERS = gql`
+  query {
     getAllUsers {
-      users
+      name
+      email
+      password
     }
   }
 `;
 
-interface Rate {
-  currency: string;
-  rate: number;
-}
+const LOGIN = gql`
+  mutation login($passwordLoginInput: PasswordLoginInput!) {
+    login(passwordLoginInput: $passwordLoginInput) {
+      access_token
+    }
+  }
+`;
 
-const App = () => {
-  const { loading, error, data } = useQuery(GetAllUsers);
+const LoginComponent = () => {
+  const [loginUser, { loading, error, data }] = useMutation(LOGIN);
+
+  const handleClick = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    try {
+      await loginUser({
+        variables: {
+          passwordLoginInput: {
+            username: "Joe",
+            password: "1234s5",
+          },
+        },
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={(e) => handleClick(e)}>Login</button>
+      {loading && <div>loading...</div>}
+      {error && <div>Error: ${JSON.stringify(error)} </div>}
+      {data && <div>{JSON.stringify(data)}</div>}
+    </div>
+  );
+};
+
+const GetAllUsersComponent = () => {
+  const { loading, error, data } = useQuery(GET_ALL_USERS);
 
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error:()</p>;
+  if (error) return <p>ERROR: ${JSON.stringify(error)}</p>;
 
-  if (data?.rates) {
-    return data.rates.map((rate: Rate) => {
-      return (
-        <div key={rate.currency}>
-          <p>
-            {rate.currency}: {rate.rate}
-          </p>
-        </div>
-      );
-    });
+  if (data) {
+    return <div>{JSON.stringify(data)}</div>;
   } else {
-    return <div>Not functioning</div>;
+    return <div>No data</div>;
   }
 };
 
+const App = () => {
+  return (
+    <div>
+      <LoginComponent />
+      <GetAllUsersComponent />
+    </div>
+  );
+};
+
 export default App;
+
+// interface Rate {
+//   currency: string;
+//   rate: number;
+// }
+
+// if (data?.rates) {
+//   return data.rates.map((rate: Rate) => {
+//     return (
+//       <div key={rate.currency}>
+//         <p>
+//           {rate.currency}: {rate.rate}
+//         </p>
+//       </div>
+//     );
+//   });
+// } else {
+//   return <div>Not functioning</div>;
+// }
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,21 @@ import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App/";
 
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+
+const link = createHttpLink({
+  uri: "http://localhost:3030/graphql",
+  credentials: "include",
+});
 
 const client = new ApolloClient({
-  uri: "http://localhost:3030/graphql",
   cache: new InMemoryCache(),
+  link,
 });
 
 const WrappedApp = () => (
